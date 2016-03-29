2ef77c9913828ca2047e638360c09d9d92f07eba
C. Morgan Hamill <me@cmhamill.org>
2016-03-28 17:07:31 -0700

    Add generic npm Makefile.

---
+++ _lib/npm.mk
@@ -0,0 +1,18 @@
+NAME = $(notdir $(CURDIR))
+
+LICENSE != npm view --json $(NAME) | jq .license
+URL != npm view --json $(NAME) | jq .homepage
+
+deb : clean
+	@echo $(NAME): building...
+	@fpm -s npm -t deb -n $(NAME) \
+	    --log error \
+	    --category web \
+	    --license $(LICENSE) \
+	    --url $(URL) \
+	    --prefix /usr \
+	    $(subst node-, , $(NAME))
+	@echo $(NAME): done.
+
+clean :
+	@rm -f -- ./$(NAME)*.deb
