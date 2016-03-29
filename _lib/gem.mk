d3a2fe12428a066ffe1dd669bd7565ee5dc4ec40
C. Morgan Hamill <me@cmhamill.org>
2016-03-28 17:07:55 -0700

    Add generic Ruby gem Makefile.

---
+++ _lib/gem.mk
@@ -0,0 +1,18 @@
+NAME = $(notdir $(CURDIR))
+
+GEM_PREFIX != gem env gemdir
+
+deb : clean
+	@echo $(NAME): building...
+	@fpm -s gem -t deb -n $(NAME) \
+	    --log error \
+	    --category ruby \
+	    --gem-package-name-prefix ruby \
+	    --gem-gem /usr/bin/gem \
+	    --prefix $(GEM_PREFIX) \
+	    --gem-bin-path /usr/bin \
+	    $(subst ruby-, , $(NAME))
+	@echo $(NAME): done.
+
+clean :
+	@rm -f -- ./$(NAME)*.deb
